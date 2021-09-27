3937737b2eda849f8b9a14dda07b834ef69148e9
NatanaelMarBa <kevin_1321124060@uptecamac.edu.mx>
2021-09-27 14:18:33 -0500

    Se añade calculadora y tablas de multiplicar

---
+++ Calculadora/main.js
@@ -0,0 +1,10 @@
+var num1 = 50;
+var num2 = 30;
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multiplicacion = num1 * num2;
+var division = num1 / num2;
+console.log("Suma: " +suma);
+console.log("Resta: " +resta);
+console.log("Multiplicacion: " +multiplicacion);
+console.log("Divicion: " +division);
